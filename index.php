bfef14906ea5392a5e0b5c9564aa09c719b26987
jhyapril <jhyapril@gmail.com>
2019-12-28 21:29:27 +0800

    added User class

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-include "includes/header.php";
+include("includes/header.php");
+include("includes/classes/User.php");
 // session_destroy();
 ?>
 
@@ -24,6 +25,11 @@ include "includes/header.php";
         <input type="submit" name="post" id="post_button" value="Post">
         <hr />
     </form>
+
+    <?php
+        $user_obj = new User($con, $userLoggedIn);
+        echo $user_obj->getFullName();
+    ?>
 </div>
 
 </div>
