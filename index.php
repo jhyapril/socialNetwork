8596e29108a1a4f8ae199f77422c587bd617122f
jhyapril <jhyapril@gmail.com>
2020-01-14 21:50:52 +0800

    Prevent resubmission of post when user refresh browser

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include("includes/classes/Post.php");
 if(isset($_POST['post'])) {
     $post = new Post($con, $userLoggedIn);
     $post->submitPost($_POST['post_text'], 'none');
+    header("Location: index.php");
 }
 $user_obj = new User($con, $userLoggedIn);
 ?>
