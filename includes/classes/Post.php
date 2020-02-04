c969892284dbde53ebc8d52659b354b9ca940ded
jhyapril <jhyapril@gmail.com>
2020-02-04 22:14:49 +0800

    - Styling post

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -81,7 +81,8 @@ class Post {
                             $body
                             <br />
                         </div>
-                    </div>";
+                    </div>
+                    <hr />";
         }
         echo $str;
     }
